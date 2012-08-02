7c7606fb719f07db6bf9fe66119b134b5f1c2ed6
Tuwuh S <tuwuhs@yahoo.com>
2012-08-02 15:24:17 +0700

    Divide superloop to several tasks. Add pH calibration function.

--- ph_meter/main.c
+++ ph_meter/main.c
@@ -42,25 +42,22 @@ static const PROGMEM unsigned char f_ph[] = {
 	0x1b,0x1b,0x1b,0x1f,0x1b,0x1b,0x1b,0x0
 };
 
+enum {
+	MODE_NORMAL,
+	MODE_CALIBRATE
+} g_operating_mode;
+
+// Calibration data
 double g_ph_const, g_ph_slope;
 
-uint8_t task_pb_sample(void)
-{
-	static uint8_t pb_pressed = 0;
-	static uint8_t pb_pin = 0;
-	uint8_t pb_flag = 0;
+//
+uint8_t g_pb_flag = 0;
+int16_t g_curr_val = 0;
+uint16_t g_stable_count = 0;
+uint8_t g_stable = 0;
 
-	// Sample pushbutton pin
-	pb_pin = PIND & 0xFC;
-
-	// Update pressed and released state
-	// Note: pb_flag is non-persistent,
-	//       the event is assumed to be handled right after the flag is set
-	pb_flag = pb_pressed & pb_pin;
-	pb_pressed = ~pb_pin;
-
-	return pb_flag;
-}
+//
+char g_lcd_string[20];
 
 void lcd_load_char(void)
 {
@@ -73,32 +70,165 @@ void lcd_load_char(void)
 	for (i = 0; i < sizeof(f_lock); i++) {
 		lcd_data(pgm_read_byte_near(&f_lock[i]));
 	}
-	lcd_command(_BV(LCD_CGRAM) | (6*8));  /* set CG RAM start address 0 */
+	lcd_command(_BV(LCD_CGRAM) | (6*8));  /* set CG RAM start address 6 */
 	for (i = 0; i < sizeof(f_ph); i++) {
 		lcd_data(pgm_read_byte_near(&f_ph[i]));
 	}
 	lcd_command(_BV(LCD_DDRAM));
 }
 
-void ph_calibrate(void)
+void task_pb_sample(void)
 {
-	g_ph_slope = ((4.0 - 7.0) / (1474 - 2113));
-	g_ph_const = (7.0 - 2113 * g_ph_slope);
+	static uint8_t pb_pressed = 0;
+	static uint8_t pb_pin = 0;
+
+	// Sample pushbutton pin
+	pb_pin = PIND & 0xFC;
+
+	// Update pressed and released state
+	// Note: pb_flag is non-persistent,
+	//       the event is assumed to be handled right after the flag is set
+	g_pb_flag = pb_pressed & pb_pin;
+	pb_pressed = ~pb_pin;
+}
+
+void task_stability_indicator(void)
+{
+	static int16_t centre_val = 0;
+
+	if (abs(centre_val - g_curr_val) < STABLE_ERROR) {
+		if (g_stable_count > STABLE_WAIT) {
+			g_stable = 1;
+			g_stable_count = 0;
+			centre_val = g_curr_val;
+		} else {
+			g_stable_count++;
+		}
+	} else {
+		g_stable = 0;
+		g_stable_count = 0;
+		centre_val = g_curr_val;
+	}
+}
+
+void ui_ph_calibrate(void)
+{
+	static enum {
+		PH7_START, PH7_PROBE, PH7_SAVE,
+		PH4_START, PH4_PROBE, PH4_SAVE,
+		CALCULATE, VERIFY, SAVE_CAL
+	} state = PH7_START;
+
+	static int16_t raw_ph7, raw_ph4;
+	static double ph_slope, ph_const;
+
+	double ph;
+
+	// Print raw averaged value
+	sprintf(g_lcd_string, "RAW %4u", g_curr_val);
+	lcd_gotoxy(0, 1);
+	lcd_puts(g_lcd_string);
+
+	// Display stability indicator
+	lcd_gotoxy(10, 1);
+	if (g_stable) {
+		lcd_putc(5);
+	} else {
+		switch (g_stable_count / (STABLE_WAIT / 5)) {
+			case 0: lcd_putc(0); break;
+			case 1:	lcd_putc(1); break;
+			case 2:	lcd_putc(2); break;
+			case 3:	lcd_putc(3); break;
+			case 4:	lcd_putc(4); break;
+			default: lcd_putc(' ');	break;
+		}
+	}
+
+	lcd_gotoxy(0, 0);
+	switch (state) {
+	case PH7_START:
+		lcd_puts("Probe pH 7.0!   ");
+		g_stable_count = 0;
+		g_stable = 0;
+		if (g_pb_flag & PB_A) state = PH7_PROBE;
+		break;
+	case PH7_PROBE:
+		lcd_puts("Probing ph 7.0..");
+		if (g_stable) state = PH7_SAVE;
+		break;
+	case PH7_SAVE:
+		raw_ph7 = g_curr_val;
+		state = PH4_START;
+		break;
+	case PH4_START:
+		lcd_puts("Probe pH 4.0!   ");
+		g_stable_count = 0;
+		g_stable = 0;
+		if (g_pb_flag & PB_A) state = PH4_PROBE;
+		break;
+	case PH4_PROBE:
+		lcd_puts("Probing ph 4.0..");
+		if (g_stable) state = PH4_SAVE;
+		break;
+	case PH4_SAVE:
+		raw_ph4 = g_curr_val;
+		state = CALCULATE;
+		break;
+	case CALCULATE:
+		ph_slope = (4.0 - 7.0) / (raw_ph4 - raw_ph7);
+		ph_const = 7.0 - raw_ph7 * ph_slope;
+		state = VERIFY;
+		break;
+	case VERIFY:
+		ph = ph_const + ph_slope * g_curr_val;
+		sprintf(g_lcd_string, "\06\07 %5.2f   OK?  ", ph);
+		lcd_puts(g_lcd_string);
+		if (g_pb_flag & PB_A) state = SAVE_CAL;
+		break;
+	case SAVE_CAL:
+		g_ph_const = ph_const;
+		g_ph_slope = ph_slope;
+		g_operating_mode = MODE_NORMAL;
+		break;
+	}
+
+	// Cancel button
+	if (g_pb_flag & PB_B) {
+		lcd_clrscr();
+		state = PH7_START;
+		g_operating_mode = MODE_NORMAL;
+	}
+}
+
+void ui_normal(void)
+{
+	double ph;
+
+	// Display pH
+	ph = g_ph_const + g_ph_slope * g_curr_val;
+	sprintf(g_lcd_string, "\06\07 %5.2f", ph);
+	lcd_gotoxy(0, 1);
+	lcd_puts(g_lcd_string);
+
+	// Display stability indicator
+	lcd_gotoxy(10, 1);
+	if (g_stable) {
+		lcd_putc(5);
+	} else {
+		switch (g_stable_count / (STABLE_WAIT / 5)) {
+			case 0: lcd_putc(0); break;
+			case 1:	lcd_putc(1); break;
+			case 2:	lcd_putc(2); break;
+			case 3:	lcd_putc(3); break;
+			case 4:	lcd_putc(4); break;
+			default: lcd_putc(' ');	break;
+		}
+	}
 }
 
 int main(void)
 {
-	char lcd_string[20];
-	int16_t curr_val = 0;
-	int16_t centre_val = 0;
-
-	uint16_t stable_count = 0;
-	uint8_t stable = 0;
-
 	uint8_t led_state = 0;
-	uint8_t pb_event = 0;
-
-	double ph;
 
 	// Set PORTD for LEDs and pushbuttons
 	// Turn-off LEDs, enable pull-up
@@ -107,7 +237,6 @@ int main(void)
 
 	// Initialize the LCD
 	lcd_init(LCD_DISP_ON);
-	lcd_puts("Hello World!");
 	lcd_load_char();
 
 	// Start ADC sampling
@@ -115,70 +244,66 @@ int main(void)
 	adc_start(2);
 	sei();
 
+	// Initialize default calibration value
 	g_ph_slope = 0.0;
 	g_ph_const = 0.0;
 
 	while (1) {
-		curr_val = (int16_t) g_adc_val;
+		// -------------------------------------
+		// ------------ BASE SYSTEM ------------
+		// -------------------------------------
+		// The base system outputs the following global vars:
+		// - g_curr_val: current ADC reading
+		// - g_stable: stability indicator
+		// - g_stable_count: stability progress counter
+		// - g_pb_flag: pushbutton event
+		// The tasks are allowed to access global variables directly
 
-		// Print raw averaged value
-		sprintf(lcd_string, "%4u - %7lu", curr_val, g_adc_avg);
-		lcd_gotoxy(0, 0);
-		lcd_puts(lcd_string);
+		// Get (averaged oversampled) ADC reading
+		g_curr_val = (int16_t) g_adc_val;
 
 		// Stability indicator
-		if (abs(centre_val - curr_val) < STABLE_ERROR) {
-			if (stable_count > STABLE_WAIT) {
-				stable = 1;
-				stable_count = 0;
-				centre_val = curr_val;
-			} else {
-				stable_count++;
-			}
-		} else {
-			stable = 0;
-			stable_count = 0;
-			centre_val = curr_val;
-		}
-
-		// Display pH
-//		ph = 7.0 + (curr_val - 2113) * (7.0 - 4.0) / (2113 - 1474);
-		ph = g_ph_const + g_ph_slope * curr_val;
-		sprintf(lcd_string, "\006\007 %5.2f   %5d",
-				ph, centre_val);
-		lcd_gotoxy(0, 1);
-		lcd_puts(lcd_string);
-
-		// Display stability indicator
-		lcd_gotoxy(10, 1);
-		if (stable) {
-			lcd_putc(5);
-		} else {
-			switch (stable_count / (STABLE_WAIT / 5)) {
-				case 0: lcd_putc(0); break;
-				case 1:	lcd_putc(1); break;
-				case 2:	lcd_putc(2); break;
-				case 3:	lcd_putc(3); break;
-				case 4:	lcd_putc(4); break;
-				default: lcd_putc(' ');	break;
-			}
-		}
+		task_stability_indicator();
 
 		// Sample pushbutton
-		pb_event = task_pb_sample();
+		task_pb_sample();
+
+		// ------------------------------------
+		// ---------- USER INTERFACE ----------
+		// ------------------------------------
+		// The ui_...() functions are responsible to:
+		// - Display to LCD
+		// - Handle pushbutton events
+		// - Update LED if any
+		// - Determine next operating mode
+
+		switch (g_operating_mode) {
+		case MODE_NORMAL:
+			ui_normal();
+			break;
+		case MODE_CALIBRATE:
+			ui_ph_calibrate();
+			break;
+		default:
+			g_operating_mode = MODE_NORMAL;
+			break;
+		}
 
 		// Handle pushbutton events
-		if (pb_event & PB_A) {
+		if (g_pb_flag & PB_A) {
 			led_state ^= 0xFF;
 		}
-		if (pb_event & PB_LEFT) {
+		if (g_pb_flag & PB_LEFT) {
 			lcd_command(LCD_MOVE_DISP_LEFT);
 		}
-		if (pb_event & PB_RIGHT) {
+		if (g_pb_flag & PB_RIGHT) {
 			lcd_command(LCD_MOVE_DISP_RIGHT);
 		}
-		if (pb_event & PB_UP) {
-			ph_calibrate();
+		if (g_pb_flag & PB_UP) {
+			g_operating_mode = MODE_CALIBRATE;
+		}
+		if (g_pb_flag & PB_DOWN) {
+			g_operating_mode = MODE_NORMAL;
 		}
 
 		// Update LED state
